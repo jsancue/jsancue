34b1469daf4ef397c5b09d80f94abe2a838db423
Jesús Sánchez Cuevas <92477712+jsancue@users.noreply.github.com>
2022-02-25 01:07:38 +0100

    Add files via upload

---
+++ IES_MAR_DE_CADIZ_WEB/js/script.js
@@ -0,0 +1,36 @@
+let navbar = document.querySelector('.header .navbar');
+let btnRegister = document.querySelector('.formularios .register-btn');
+let btnLogin = document.querySelector('.formularios .login-btn');
+let formularios = document.querySelector('.formularios');
+let formLogin = document.querySelector('.formularios .login-form');
+let formRegister = document.querySelector('.formularios .register-form');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('activo');
+};
+
+document.querySelector('#cerrar-menu').onclick = () =>{
+    navbar.classList.remove('activo');
+};
+
+document.querySelector('#cuenta-btn').onclick = () =>{
+    formularios.classList.add('activo');
+};
+
+document.querySelector('#cerrar-form').onclick = () =>{
+    formularios.classList.remove('activo');
+};
+
+btnRegister.onclick = () =>{
+    btnRegister.classList.add('activo');
+    btnLogin.classList.remove('activo');
+    formLogin.classList.remove('activo');
+    formRegister.classList.add('activo');
+};
+
+btnLogin.onclick = () =>{
+    btnRegister.classList.remove('activo');
+    btnLogin.classList.add('activo');
+    formLogin.classList.add('activo');
+    formRegister.classList.remove('activo');
+};
